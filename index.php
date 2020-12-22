d65df4550c9561dd0afba3726519a32af4919e92
Emanuel <e.rista@outlook.it>
2020-12-22 17:18:42 +0100

    fixed

--- index.php
+++ index.php
@@ -8,10 +8,13 @@ $badword = $_GET["badword"];
 
 $text = "Il cavolo si adopera innanzitutto come affermazione, quando ti chiedono qualcosa. Ad esempio: 'Li hai già fatti i compiti delle vacanze?'. Risposta: 'cavolo!'. Oppure: 'Hai rinnovato l'abbonamento del treno?'. Risposta: 'cavolo!'. O anche: 'Ti sei ricordato di lavarti le ascelle?'. Risposta: 'cavolo!'. Tuttavia, grazie alla sua versatilità, il 'cavolo' si può pure usare come negazione. Nello specifico: 'Che hai mangiato oggi?'. Risposta: 'Un cavolo'. Oppure: 'Ci hai capito qualcosa delle spiegazione?' Risposta: 'Un cavolo'. O perfino: 'Che cosa vedi all’orizzonte?' Risposta: 'Un cavolo'. E via dicendo. Esso però funziona parimenti come interrogativo, nel caso di 'cavolo fai?', 'cavolo vai?', 'cavolo vuoi?', 'cavolo sei?', 'cavolo è?', ma anche 'cavolo dici?', 'cavolo rompi?', 'cavolo parli?', eccetera. In definitiva, il cavolo lo si può mettere praticamente dappertutto.";
 
-$text = str_replace($badword, "***", $text);
+$censored_text = str_replace($badword, "***", $text);
 
 
 ?>
 
 <p> <?php echo ($text)  ?> </p>
 <p> <?php echo ("Questo paragrafo è lungo " . strlen($text) . " caratteri")  ?> </p>
+
+<p> <?php echo ($censored_text)  ?> </p>
+<p> <?php echo ("Questo paragrafo è lungo " . strlen($censored_text) . " caratteri")  ?> </p>
